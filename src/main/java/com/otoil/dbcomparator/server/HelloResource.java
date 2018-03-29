4aea1ceaa5b744933625aeb83473bc11af313a83
Sergey Medelyan <fakeAcc18@yandex.ru>
2018-03-29 19:54:41 +0300

    added test hello rest service

--- src/main/java/com/otoil/dbcomparator/server/HelloResource.java
+++ src/main/java/com/otoil/dbcomparator/server/HelloResource.java
@@ -1,19 +1,19 @@
 package com.otoil.dbcomparator.server;
 
-//import javax.servlet.http.HttpServlet;
-//import javax.ws.rs.GET;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.Produces;
-//
-//@Path("/")
-//public class HelloResource extends HttpServlet
-//{
-//    @GET
-//    @Path("sayhello")
-//    @Produces("text/plain")
-//    public String getHello()
-//    {
-//        return "hello";
-//    }
-//}
-public class HelloResource {}
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+
+@Path("/")
+public class HelloResource
+{
+    @GET
+    @Path("sayhello")
+    @Produces("text/plain")
+    public String getHello()
+    {
+        return "hello";
+    }
+}
